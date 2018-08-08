90120c8ca8bfec43f55f2202d2ad59823ffdc1eb
cwooz <woosley.christopher@gmail.com>
2018-08-08 15:37:32 -0400

    bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -25,53 +25,70 @@ class App extends Component {
     // const { classtwo } = {  };
 
     return (
-      <div className="App">
-        {/* Render the passwords */}
-        {passwords.length ? (
-          <div>
-            <h1>5 Passwords.</h1>
-            <ul className="passwords">
-              {/* There will always be the same number of passwords, and they never change positions in the array */}
-              {passwords.map((password, index) =>
-                <li key={index}>
-                  {password}
-                </li>
-              )}
-            </ul>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Get More
-            </button>
 
-              <a 
-                className="btn btn-primary"
-                role="button"
-                href="https://github.com/cwooz/demoReactApp"
-                target="_blank">
-                <i
-                  className="material-icons link">
-                </i>
-                  Check the Code
+      <div className="jumbotron">
+        <div className="App">
+          {/* Render the passwords */}
+          {passwords.length ? (
+            <div>
+              <h1>5 Passwords.</h1>
+              <ul className="passwords">
+                {/* There will always be the same number of passwords, and they never change positions in the array */}
+                {passwords.map((password, index) =>
+                  <li key={index}>
+                    {password}
+                  </li>
+                )}
+              </ul>
+              <button
+                className="more"
+                onClick={this.getPasswords}>
+                Get More
+              </button>
+              <br/>
+              <br/>
+
+            <div className="dropdown">
+              <a className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown 4 Links
               </a>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                  <a className="dropdown-item" href="https://github.com/cwooz/demoReactApp" target="_blank">View the Code</a>
+                  <a className="dropdown-item" href="https://www.npmjs.com/package/password-generator" target="_blank">NPM/password-generator</a>
+                </div>
+              </div>
 
-            {/* <a 
-              href="https://github.com/cwooz/demoReactApp"
-              onClick={handleClick}>
-              View the Code
-            </a> */}
-          </div>
-        ) : (
-          // Render a helpful message otherwise
-          <div>
-            <h1>No passwords :(</h1>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Try Again?
-            </button>
-          </div>
-        )}
+
+
+                {/* <a 
+                  className="btn btn-primary"
+                  role="button"
+                  href="https://github.com/cwooz/demoReactApp"
+                  target="_blank">
+                  <i
+                    className="material-icons link">
+                  </i>
+                    View the Code
+                </a> */}
+
+              {/* <a 
+                href="https://github.com/cwooz/demoReactApp"
+                onClick={handleClick}>
+                View the Code
+              </a> */}
+            </div>
+          ) : (
+            // Render a helpful message otherwise
+            <div>
+              <h1>No passwords :(</h1>
+              <button
+                className="more"
+                onClick={this.getPasswords}>
+                Try Again?
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     );
   }
